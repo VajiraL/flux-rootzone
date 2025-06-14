7d1860c84fc374ef2d5b456e5979b133e6c51c40
Vajira Lasantha <vajira.lasantha@outlook.com>
2025-06-14 16:15:32 +0000

    minor

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
----
-editor_options: 
-  markdown: 
-    wrap: 72
----
-
 # Exploring water-ecosystem dynamics at flux sites
 
 This repo contains exploratory analyses into water-ecosystem dynamics
--- analysis/10_download_data.R
+++ analysis/10_download_data.R
@@ -1,4 +1,18 @@
-## Download data
-
+## Download data 
 # execute the download bash script
 source("src/download_FluxDataKit.sh")
+
+# remotes::install_local("/home/vajira/R/FluxDataKit-main.zip", dependencies = TRUE)
+# set working directory to the root of the project
+setwd("/home/vajira/flux-rootzone/")
+
+## list of sites available as a csv
+sites <- read.csv("data/fdk_site_info.csv", stringsAsFactors = FALSE)|>
+  filter(!(sitename %in% c("MX-Tes", "US-KS3"))) |>  # failed sites
+  filter(!(igbp_land_use %in% c("CRO", "WET"))) |> 
+  left_join(
+    fdk_site_fullyearsequence,
+    by = "sitename"
+  ) |> 
+  filter(!drop_gpp) |>  # where no full year sequence was found
+  filter(nyears_gpp >= 3)
